557152fbbc1cd8cabcd9a3af1d5a4cf8e6e467c2
Luis Sevilla <user.sevilu6@gmail.com>
2022-07-21 12:33:06 -0500

    pushed to heroku

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 
-const PORT = 3000;
+const { PORT = 3000 } = process.env;
 const express = require('express');
 const server = express();
 
@@ -13,15 +13,14 @@ server.use((req, res, next) => {
     console.log("<____Body Logger START____>");
     console.log(req.body);
     console.log("<_____Body Logger END_____>");
-    
+
     next();
 });
 
 server.get('/add/:first/to/:second', (req, res, next) => {
-    res.send(`<h1>${ req.params.first } + ${ req.params.second } = ${
-      Number(req.params.first) + Number(req.params.second)
-     }</h1>`);
-  });
+    res.send(`<h1>${req.params.first} + ${req.params.second} = ${Number(req.params.first) + Number(req.params.second)
+        }</h1>`);
+});
 
 const apiRouter = require('./api');
 server.use('/api', apiRouter);
